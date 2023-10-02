da2ade6e9caead48379c8226c93b6adeb856c402
Dinyad <donatien.yeto@gmail.com>
2023-10-02 10:04:08 +0200

    First deploy

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'smart-plant-fbfdb',
+    appId: '1:990794259823:web:d8502330d42cdc6b1b5c9a',
+    databaseURL: 'https://smart-plant-fbfdb-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'smart-plant-fbfdb.appspot.com',
+    apiKey: 'AIzaSyD-z6Lj9U-UfCEFQF2Ik-8qnGjV1WYFYW4',
+    authDomain: 'smart-plant-fbfdb.firebaseapp.com',
+    messagingSenderId: '990794259823',
+  },
+};
