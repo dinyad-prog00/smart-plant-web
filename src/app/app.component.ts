8b57dfd565ef5cfa37d1855764a70ca544aba5d1
Dinyad <donatien.yeto@gmail.com>
2024-02-11 14:56:58 +0100

    chore: clean graph init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Measurement } from 'src/models';
 import { AngularFireDatabase, AngularFireList } from "@angular/fire/compat/database"
 import { map } from 'rxjs/operators';
+import * as moment from 'moment';
 import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
 
 @Component({
@@ -27,14 +28,20 @@ export class AppComponent {
     //       },
     //     },
     //   },
-      
+
     // },
   };
   public lineChartLegend = true;
 
+
+
   constructor(db: AngularFireDatabase) {
-    this.historyRef = db.list<Measurement>("/measurements")
+    const path = "mesures-final/history"
+    this.historyRef = db.list<Measurement>(path)
   }
+
+  formatDate = (dt: Date) => moment(dt) .format("MMM D HH:mm");
+
   ngOnInit(): void {
     this.historyRef?.snapshotChanges()
       .pipe(
@@ -46,12 +53,13 @@ export class AppComponent {
         })
       )
       .subscribe((data) => {
+        let len = data.length
         this.history = data;
         this.humidity = {
-          labels: data.map((it) => it.date).slice(0, 150),
+          labels: data.map((it) => it.date).slice(len - 50).map(this.formatDate),
           datasets: [
             {
-              data: data.map((it) => it.humidity).slice(0, 150) as number[],
+              data: data.map((it) => it.humidity).slice(len - 50) as number[],
               label: 'Humidity',
               fill: true,
               tension: 0.5,
@@ -64,10 +72,10 @@ export class AppComponent {
         }
 
         this.ph = {
-          labels: data.map((it) => it.date).slice(0, 150),
+          labels: data.map((it) => it.date).slice(len - 150).map(this.formatDate),
           datasets: [
             {
-              data: data.map((it) => it.ph).slice(0, 150) as number[],
+              data: data.map((it) => it.ph).slice(len - 150) as number[],
               label: 'PH',
               fill: true,
               tension: 0.5,
@@ -83,3 +91,48 @@ export class AppComponent {
       })
   }
 }
+
+
+// let date = new Date("2023-10-05T10:38:18.125Z")
+// let i = 299;
+
+// const simulateSend = () => {
+
+
+//   date.setMinutes(date.getMinutes() + (i * 5))
+//   const data = {
+//     i: {
+//       "date": date,
+//       "humidity": (Math.round((Math.random() * 20))),
+//       "lightRadiation": (Math.round((Math.random() * 30))),
+//       "nitrateContent": (Math.round((Math.random() * 20))),
+//       "ph": (Math.round((Math.random() * 14))),
+//       "potassiumContent": (Math.round((Math.random() * 16))),
+//       "saltContent": (Math.round((Math.random() * 15)))
+//     }
+//   };
+
+
+//   const requestConfig = {
+//     method: 'PATCH',
+//     url: 'https://smart-plant-fbfdb-default-rtdb.europe-west1.firebasedatabase.app/test.json',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       // Add any other headers if needed
+//     },
+//     body: {
+//       mode: 'raw',
+//       raw: JSON.stringify(data) // Replace with your request payload
+//     }
+//   };
+
+//   pm.sendRequest(requestConfig, function (error, response) {
+//     if (error) {
+//       console.error('Error:', error);
+//     } else {
+//       console.log('Response:', response.text());
+//     }
+//   });
+// }
+
+// setInterval(simulateSend, 300000);
