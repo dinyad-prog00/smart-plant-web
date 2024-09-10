77eb9dc02a110e4683ef00581c29a447228b1b7d
Donatien Dinyad Yeto <76538764+dinyad-prog00@users.noreply.github.com>
2024-09-10 09:14:43 +0200

    Update environment.ts - rm apiKey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     appId: '1:990794259823:web:d8502330d42cdc6b1b5c9a',
     databaseURL: 'https://smart-plant-fbfdb-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'smart-plant-fbfdb.appspot.com',
-    apiKey: 'AIzaSyD-z6Lj9U-UfCEFQF2Ik-8qnGjV1WYFYW4',
+    apiKey: 'xxxxxxxxx',
     authDomain: 'smart-plant-fbfdb.firebaseapp.com',
     messagingSenderId: '990794259823',
   },
